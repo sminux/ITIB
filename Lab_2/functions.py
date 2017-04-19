85bd75ce97f2d2089113cd8237429856d56509eb
sminux <espin55@yandex.ru>
2017-04-19 23:30:00 +0300

    +matprotlib for grafics

--- Lab_2/functions.py
+++ Lab_2/functions.py
@@ -1,53 +1,54 @@
 '''
 Created on 4 апр. 2017 г.
-
 @author: Сергей
 '''
 import math
+import matplotlib.pyplot as plotF
 
-p = 9
-X = []*(p+1)
+p = 11
+X = [0]*40
+d = 0
+w = [0.0]*(p + 1)
 a = -2
 b = 2
-n = 0.5 
-        
-def prediction(X, t, w):    
+n = 0.5
+
+def prediction(X, w, t):    
     net = 0
-    delta = 0.0
+    d = 0.0
     net += sum(X[i]*w[i] for i in range(p+1))
-    delta = t - net
+    d = t - net
     i = 0
-    for i in range(p+1):
-        w[i] += (delta * n * X[i])
-    return delta ** 2
+    for i in range(p + 1):
+        w[i] += (d * n * X[i])
+    return d ** 2
 
 def net(X, w):
     net = 0
-    net += sum(X[i]*w[i] for i in range(p+1))
+    net += sum(X[i]*w[i] for i in range(p + 1))
     return net
 
-
-X = [0]*(p + 1)
-
 def learning(num_of_epos):   
-    n = (b - a) * 4 + 1 
-    F = [0]*n
+    N = 20
+    F = []
     i = 0
-    for i in range(n):
-        F[i] = math.sin((a + i - 1))#0.4 * math.sin(0.3 * (a + (0.25 * i))) - 0.5 
+    for i in range(N):
+        F.append(math.sin((a + i - 1)))
+        i += 0.25
+        
     print()
     print("Number of epos: ", num_of_epos)
+    
     epo = 0
     for epo in range(num_of_epos):
         e = 0.0
         i = 0
         j = 1
-        for i in range(n - p):
+        for i in range(N - p):
             for j in range(p + 1):
-                X[0] = 1
                 X[j] = F[i + j - 1]
-            w = [0]*(p+1) 
-            d = prediction(X, F[i + p], w)/(n - p)
+            w = [0]*(p + 1) 
+            d = prediction(X, w, F[i + p])/(N - p)
             e += d
         
     print("ε = ", math.sqrt(e))
@@ -55,16 +56,67 @@ def learning(num_of_epos):
     for i in range(p):
         print('w[%s] = %.3f' %(i, w[i]), end=' ')
         
-    x = b
-    i = n - p 
+    x = a
+    i = N - p 
     j = 1
     print("\nx:       y:       σ:")
-    for i in range(n + n - p - 1):
+    for i in range(2*N - p - 1):
         for j in range(p + 1):
             X[0] = 1
             X[j] = F[i + j - 1]
         y = net(X, w)
-        #x += 0.25
-        mistake = (math.sin(x) - 1) - y#0.4 * math.sin(0.3 * (0.25 * x)) - 0.5 
+        x += 0.25
+        mistake = (math.sin(x) - 1) - y
         F.append(y)
-        print("%.3f   %.3f   %.3f" %(x, y, mistake))
+        print(y)
+        #print("%.2f   %.7f   %.7f" %(x, y, mistake))
+     
+  
+  
+  
+  
+      
+'''
+def learning(epos):
+    e = 0.00000
+    ep = 0
+    for ep in range(epos):
+        i = 0
+        j = 0
+        for i in range(20):
+            X[i] = math.sin(a + (i*(b - a)/20) - 1) #(b - a)/20 - равное количество шагов в интервал 
+         
+        i = 0
+        for j in range(ep):
+            for i in range(20 - p):
+                net = 0.0
+                net = sum(X[i + itern]*w[itern] for itern in range(p))
+                d[i] = X[i + p] - net
+                
+                num = 0
+                if d != 0:
+                    for num in range(p):
+                        w[num] += X[i + num] * d[i] * n 
+    
+        e = 0
+        i = 20 - p    
+        for i in range(40 - p):
+            net = 0.0
+            for itern in range(p):
+                net += X[i + itern]*w[itern]
+            X[i + p] = net
+            y[i + p - 20] = math.sin(b + (i + p - 20)*(b - a)/20) 
+            f = y[i + p - 20] - X[i + p] 
+            e += f ** 2
+               
+        i = 20
+        for i in range(40):
+            print('ε = %.7f' %(math.sqrt(e))) 
+              
+        w = [0]*10
+        f = 0
+        e = 0
+        net = 0
+    #plotF.plot(X, y)  
+    #plotF.show()             
+    '''
--- Lab_2/main.py
+++ Lab_2/main.py
@@ -1,19 +1,92 @@
 '''
 Created on 3 апр. 2017 г.
-
-@author: Сергей
+@author: sminux
 '''
-import math 
-import functions
+import math
+import matplotlib.pyplot
 
 print('ITIB Laboratory work #2. Application of a single-layer neural network with a linear activation function for the prediction of time series')
 
-Epos_first = 2000
-Epos_second = 4000
+W = [0]*15
+X = [0]*40
+y = [0]*20
+d = [0]*20
+p = 11
+n = 0.1
+a = -5.0
+b = 3.0
+f = 0.0
+eps = 0.0
+net = 0.0
+step = (b - a) / 20
+for p in range(15): 
+    k = 0
+    era = 25
+    j = 0
 
-functions.learning(Epos_first)
-functions.learning(Epos_second)
+    for j in range(20):
+        X[j] = math.sin(a + j*step - 1)
+    j = 0
+    i = 0
+    for j in range(era):
+        for i in range(20-p):
+            net = 0.0
+            o = 0
+            for o in range(p):
+                net += W[o] * X[i + o]
+            d[i] = X[i + p] - net   
+            u = 0
+            if d[i] != 0:
+                for u in range(p):
+                    W[u] = W[u] + X[i + u] * d[i] * n
+
+    i = 0
+    for i in range(20):
+        print('X: %.6f' %(X[i]))
+    print('W:')
+    for i in range(p):
+        print('[%.3f' %(W[i]), end=']')
     
+    i = 20 - p
+    for i in range(40 - p):
+        net = 0.0
+        o = 0
+        for o in range(p):
+            net = net + W[o] * X[i + o]
+        X[i + p] = net
+        y[i + p - 20] = math.sin(b + (i + p - 20)*step - 1)
+        f = y[i + p - 20] - X[i + p]
+        eps = eps + f**2
+
+    i = 20
+    for i in range(40):
+        #print('X[', i, ']: ', X[i],'| Y:', y[i - 20] )
+        #print('ε =', math.sqrt(eps))
+        print(y[i - 20])
     
-    
- 
+    f = 0.0
+    eps = 0.0
+    net = 0.0
+    m = 0
+    for m in range(p):
+        W[m] = 0
+
+x =[]
+'''
+y_norm = []
+for i in range(20):
+    x.append(a + i*step)
+    y_norm.append(math.sin(a + i*step - 1))
+matplotlib.pyplot.plot(x, y_norm) 
+matplotlib.pyplot.xlabel(r'$x$') 
+matplotlib.pyplot.ylabel(r'$f(x) non-prediction$') 
+matplotlib.pyplot.grid(True) 
+matplotlib.pyplot.show() 
+'''
+for i in range(20):
+    x.append(a + i*step)
+matplotlib.pyplot.plot(x, y) 
+matplotlib.pyplot.xlabel(r'$x$') 
+matplotlib.pyplot.ylabel(r'$f(x) prediction$') 
+matplotlib.pyplot.grid(True) 
+matplotlib.pyplot.show() 
